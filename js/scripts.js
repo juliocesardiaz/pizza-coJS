f9b16ed2cdf94c43ad2c2b473f25caf9d1d05c7c
lujxio <jcdiaz92@gmail.com>
2015-09-18 16:10:49 -0700

    writes html and fixes bugs

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,37 @@ Pizza.prototype.calculatePrice = function(){
 };
 
 $(document).ready(function() {
+    var newPizza;
+    $("#add-toppings").click(function(){
+        $("#new-toppings").append('<label for="topping">Topping</label>' +
+									'<select name="topping" id="topping">' +
+										'<option value="cheese">Cheese</option>' +
+										'<option value="pepperoni">Pepperoni</option>' +
+										'<option value="mushrooms">mushrooms</option>' +
+										'<option value="pinepple">pinepple</option>' +
+										'<option value="sausage">sausage</option>' +
+										'<option value="ham">ham</option>' +
+										'<option value="chicken">chicken</option>' +
+									'</select>' +
+								'<br>')
+    });
+    
+    $("form#new-pizza").submit(function(event) {
+       event.preventDefault();
+       
+       var inputtedQuantity = parseInt($("select#quantity").val());
+       var inputtedTopping = $("select#first-topping").val(); 
+       var inputtedSize = $("select#size").val();
+       
+       newPizza = new Pizza(inputtedQuantity, inputtedTopping, inputtedSize);
+       
+       $(".new-toppings").each(function() {
+			var inputtedTopping2 = $("select#topping").val(); 
+            newPizza.addTopping(inputtedTopping2);
+		});
+       
+       $(".price").fadeOut();
+       $(".price").fadeIn(1000);
+       $(".pizza-price").text(newPizza.calculatePrice());
+    });
 });
