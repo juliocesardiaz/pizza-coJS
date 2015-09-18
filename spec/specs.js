8cc2c85a84884e3fb5e3143813b02272449b028c
lujxio <jcdiaz92@gmail.com>
2015-09-18 14:18:35 -0700

    passes first couple specs

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,19 @@ describe('Pizza', function() {
         var testPizza = new Pizza();
         expect(testPizza).to.eql(new Pizza());
     });
+    
+    it("Check quantity of Pizza", function() {
+        var testPizza = new Pizza(2, "cheese", "s");
+        expect(testPizza.quantity).to.equal(2);
+    });
+    
+    it("Check toppings of Pizza", function() {
+        var testPizza = new Pizza(2, "cheese", "s");
+        expect(testPizza.toppings).to.eql(["cheese"]);
+    });
+    
+    it("Check size of Pizza", function() {
+        var testPizza = new Pizza(2, "cheese", "s");
+        expect(testPizza.size).to.equal("s");
+    });
 });
